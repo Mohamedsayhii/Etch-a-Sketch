3f05aad7a952fe3e4a7b6681c2648ca3a754e95f
Mohamedsayhii <mohamedsaihii@outlook.fr>
2021-12-09 20:03:08 +0100

    added the hovering effect

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ let columns = 16;
 
 let gridContainer = document.querySelector(".grid-container");
 
-gridContainer.style.gap = `3px`;
-
 for (let i = 1; i <= rows * columns; i++) {
   let gridItem = document.createElement("div");
   let gridItemStyle = gridItem.style;
-  gridItemStyle.border = `1px solid`;
-  // gridItem.style.margin = `0px`;
-  // gridItem.style.padding = `0px`;
+  // gridItemStyle.border = `1px solid`;
+  gridItem.style.margin = `0px`;
+  gridItem.style.padding = `0px`;
   gridContainer.appendChild(gridItem);
 }
+
+let gridItems = document.querySelectorAll(`.grid-container div`);
+
+gridItems.forEach((gridItem) => {
+  gridItem.addEventListener(`mousemove `, () => {
+    gridItem.style.backgroundColor = `orange`;
+  });
+});
