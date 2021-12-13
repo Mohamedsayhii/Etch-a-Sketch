8af750a6e57ce67f5ea740219861f07f9ba4731e
Mohamedsayhii <mohamedsaihii@outlook.fr>
2021-12-13 13:49:01 +0100

    yalla 7ouma

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let rows;
 let columns;
 
+let colorBtn = document.querySelector(`#color-btn`);
+let rainbow = document.querySelector(`#rainbow-btn`);
+let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
 //Steps 2 and 3
 let gridContainer = document.querySelector(".grid-container");
 
@@ -27,9 +31,7 @@ resetButton.addEventListener(`click`, () => {
   size = Number(prompt(`Enter the new size`));
   console.log(size);
 
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.lastChild);
-  }
+  gridContainer.innerHTML = "";
 
   gridContainer.style.gridTemplateColumns = `repeat(${size}, minmax(0, 1fr))`;
   gridContainer.style.gridTemplateRows = `repeat(${size}, minmax(0, 1fr))`;
@@ -43,7 +45,7 @@ resetButton.addEventListener(`click`, () => {
 
   gridItems.forEach((gridItem) => {
     gridItem.addEventListener(`mousemove`, () => {
-      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
       gridItem.style.backgroundColor = `#` + randomColor;
       gridItem.style.transitionDuration = `1.2s`;
     });
